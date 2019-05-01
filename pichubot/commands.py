2dfdef50b0b70ec8f7ff2082ec69c94d935debf0
Anthony Luc <anthonyvanluc@gmail.com>
2019-05-01 18:20:45 -0400

    add roll command

--- pichubot/commands.py
+++ pichubot/commands.py
@@ -7,6 +7,8 @@ Each function is its own command.
 
 from __future__ import print_function
 
+from random import randrange
+
 from discord.ext import commands
 
 """pichu
@@ -32,3 +34,19 @@ async def echo(ctx, msg=None):
         await ctx.send(msg)
     else:
         await ctx.send('Missing parameter!')
+
+"""roll
+
+Roll a dice
+
+Args:
+    sides (str): passed in number of sides
+
+"""
+@commands.command(pass_context=True)
+async def roll(ctx, sides=None):
+    if sides is None:
+        sides = 6
+    # TODO: no handling of user error if sides is a word not number
+    # maybe abstract an exception catcher
+    await ctx.send(randrange(int(sides))+1)
